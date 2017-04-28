eb6267a6e5dd43950c13b60c0e082b2073030c98
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-04-28 15:29:13 -0700

    connect basic client to basic server

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
 ReactDOM.render(
   <App />,
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static('./client/dist'))
+
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
 
 app.listen(1935, function () {
   console.log('Monopoly Money Manager listening on port 1935!')
