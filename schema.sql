8aa2238777f3216d30bd341fd41ecaceaff52b7f
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-04-29 20:43:49 -0700

    add schema

---
+++ schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS mmm;
+
+CREATE DATABASE mmm;
+
+USE mmm;
+
+CREATE TABLE players (
+  id int NOT NULL AUTO_INCREMENT,
+  token string NOT NULL,
+  tokenImg string NOT NULL,
+  balance int NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO players (token, tokenImg, balance) VALUES ('bank', 'bank', 150000);
+INSERT INTO players (token, tokenImg, balance) VALUES ('hat', 'hat', 1500);
+INSERT INTO players (token, tokenImg, balance) VALUES ('trex', 'trex', 1500);
